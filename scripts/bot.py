d4e599cbc29d88eb867f250da06685882f17aa2d
synder <tobyred8@hotmail.co.uk>
2024-05-20 23:28:00 +0100

    getting the ball rolling

--- scripts/bot.py
+++ scripts/bot.py
@@ -1,3 +1,18 @@
+import os
 import discord
 from discord.ext import commands
 
+# Read the token from the environment variable
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+# Set up the bot
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Define a simple command
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+# Run the bot
+bot.run(TOKEN)
